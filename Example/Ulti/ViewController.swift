e9396d5ed683b784e5eefc109d828e097f7daeaa
許湘潔 <jessiehsu@candyhouse.co>
2022-11-04 15:11:20 +0800

    Initial commit

---
+++ Example/Ulti/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Ulti
+//
+//  Created by 82069976 on 11/04/2022.
+//  Copyright (c) 2022 82069976. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
